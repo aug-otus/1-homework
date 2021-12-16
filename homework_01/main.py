29fbc625d421c61d4a8b58843adfea579711addd
aug-otus <96004697+aug-otus@users.noreply.github.com>
2021-12-16 20:21:51 +0300

    edit homework

--- homework_01/main.py
+++ homework_01/main.py
@@ -5,15 +5,13 @@
 
 
 def power_numbers(*num, power=2):
-    return [x ** power for x in num]
     """
     функция, которая принимает N целых чисел,
     и возвращает список квадратов этих чисел
     >>> power_numbers(1, 2, 5, 7)
     <<< [1, 4, 25, 49]
     """
-
-print ("power",power_numbers(1,2,5,7))
+    return [x ** power for x in num]
 
 
 
@@ -34,26 +32,29 @@ def is_prime(prime):
 
 
 def filter_numbers(num, fil):
-        if fil == 'odd':
-                return list(filter(lambda x: x % 2 and x > 0, num))
-        if fil == 'even':
-                return list(filter(lambda x: x % 2 == 0 and x > 0, num))
-        if fil == 'prime':
-            return list(filter(lambda x: x == is_prime(x), num))
+    """
+    функция, которая на вход принимает список из целых чисел,
+    и возвращает только чётные/нечётные/простые числа
+    (выбор производится передачей дополнительного аргумента)
 
-        """
-        функция, которая на вход принимает список из целых чисел,
-        и возвращает только чётные/нечётные/простые числа
-        (выбор производится передачей дополнительного аргумента)
+    >>> filter_numbers([1, 2, 3], ODD)
+    <<< [1, 3]
+    >>> filter_numbers([2, 3, 4, 5], EVEN)
+    <<< [2, 4]
+    """
+    if fil == ODD:
+        return list(filter(lambda x: x % 2 and x > 0, num))
+    if fil == EVEN:
+        return list(filter(lambda x: x % 2 == 0 and x > 0, num))
+    if fil == PRIME:
+        return list(filter(is_prime, num))
 
-        >>> filter_numbers([1, 2, 3], ODD)
-        <<< [1, 3]
-        >>> filter_numbers([2, 3, 4, 5], EVEN)
-        <<< [2, 4]
-        """
 
-print ("odd",filter_numbers([-1,0,1,2,3,4,5,6,7,8,9,10,11],ODD))
-print ("even",filter_numbers([-1,0,1,2,3,4,5,6,7,8,9,10,11],EVEN))
-print ("prime",filter_numbers([-1,0,1,2,3,4,5,6,7,8,9,10,11],PRIME))
+
+if __name__ == "__main__":
+    print ("power",power_numbers(1,2,5,7))
+    print ("odd",filter_numbers([-1,0,1,2,3,4,5,6,7,8,9,10,11],ODD))
+    print ("even",filter_numbers([-1,0,1,2,3,4,5,6,7,8,9,10,11],EVEN))
+    print ("prime",filter_numbers([-1,0,1,2,3,4,5,6,7,8,9,10,11],PRIME))
 
 
